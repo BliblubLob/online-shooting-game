3a8bd7dc7d5e85c366a5ec6856519c3f2b8ebaf1
BliblubLob <85423227+BliblubLob@users.noreply.github.com>
2022-07-10 00:40:49 +0800

    change cors to *

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const {add_game_room,remove_game_room,add_user,get_user, create_player,get_game_
 const httpServer = http.createServer();
 const io = new socketio.Server(httpServer, {
     cors: {
-      origin: "https://unrivaled-dasik-33d573.netlify.app",
+      origin: "*",
     },
   });
 //allow other servers to access
